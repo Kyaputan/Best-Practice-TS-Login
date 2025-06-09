deb32d51525836e81c0af8220a557ee8dcb3e633
Rachata Singkhet <singkhet1@gmail.com>
2025-06-10 00:29:18 +0700

    Update

--- src/routes/adminproduct.route.ts
+++ src/routes/adminproduct.route.ts
@@ -5,7 +5,7 @@ import { requireLogin , checkRole } from '../middleware/auth.middleware';
 const router: Router = express.Router();
 
 router.post('/add' , requireLogin , checkRole({ role: "admin" }) , AdminProductController.createProduct);
-router.patch('/update' , requireLogin , checkRole({ role: "admin" }),AdminProductController.updateProduct);
-router.delete('/delete' , requireLogin , checkRole({ role: "admin" }),AdminProductController.deleteProduct);
+router.patch('/update/:id' , requireLogin , checkRole({ role: "admin" }),AdminProductController.updateProduct);
+router.delete('/delete/:id' , requireLogin , checkRole({ role: "admin" }),AdminProductController.deleteProduct);
 
 export default router;
